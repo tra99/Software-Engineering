b917afd8cb4d41fa512ca3266b900f5ad155c997
HOK Sochetra <chetrafc60@gmail.com>
2024-01-23 11:01:28 +0700

    fetch product

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,12 +1,67 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import "./Home.css"
 
-const Home = () => {
+const ProductList = () => {
+  const [productModels, setProductModels] = useState([]);
+
+  useEffect(() => {
+    // Fetch data from the API when the component mounts
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:8000/api/admin/product/list');
+        const data = await response.json();
+        setProductModels(data); 
+        console.log(response)
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []); // Empty dependency array ensures that the effect runs only once
+
   return (
     <div>
-      <h2>Welcome to the Home Page!</h2>
-      {/* Add your home page content here */}
+      <h1>Product List</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Product Code</th>
+            <th>Product Name</th>
+            <th>Origin Country</th>
+            <th>Price</th>
+            <th>Cost</th>
+            <th>Image URL</th>
+            <th>Description</th>
+            <th>Edit</th>
+          </tr>
+        </thead>
+        <tbody>
+          {productModels.map((item) => (
+            <tr key={item.code}>
+              <td>{item.code}</td>
+              <td>{item.name}</td>
+              <td>{item.country}</td>
+              <td>${item.price}</td>
+              <td>{item.cost}</td>
+              <td><img style={{width:"100px"}} src={item.image} alt="React Image" /></td>
+              <td>{item.description}</td> 
+              <td>
+                <a href={`/update-product?productCode=${item.productCode}`} className="edit-button">
+                  Edit
+                </a>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <a href="/add-product" className="add-product-btn">
+        Add Product
+      </a>
     </div>
   );
 };
 
-export default Home;
+export default ProductList;
+
+
